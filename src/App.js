3f5ebb51dc548078a1d6a79b7c07fcdb695a8e31
Akash Gupta <akash.gupta.anup@gmail.com>
2021-07-22 13:07:58 +0530

    added the main container tags in the app.js tree

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
-import "./App.css";
+import React from "react";
+import "./styles/App.css";
+import MainOuterContainer from "./components/Main_outer_container";
 
 function App() {
-  return <div className="App">hello world</div>;
+  return (
+    <div className="App">
+      <>
+        <MainOuterContainer
+          Background_color="#FFEFF1"
+          isNavbar
+        ></MainOuterContainer>
+        <MainOuterContainer Background_color="#FFFBF2" />
+      </>
+    </div>
+  );
 }
 
 export default App;
