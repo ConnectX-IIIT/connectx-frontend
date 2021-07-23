45816622b3aa3b5faa810c07a6fc50080568fe60
Akash Gupta <akash.gupta.anup@gmail.com>
2021-07-23 12:44:04 +0530

    Added Button Jsx

---
+++ src/components/Button.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "../styles/Button.css";
+
+function Button() {
+  return (
+    <div>
+      <button className="Join_us_button">Join Us -{`>`} </button>
+    </div>
+  );
+}
+
+export default Button;
