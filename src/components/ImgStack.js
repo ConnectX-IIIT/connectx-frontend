42d4259928140e9461e63e16ae82156c0e74c317
Akash Gupta <akash.gupta.anup@gmail.com>
2021-07-23 22:22:13 +0530

    created image stack

---
+++ src/components/ImgStack.js
@@ -0,0 +1,34 @@
+import React, { useState } from "react";
+import "../styles/ImgStack.css";
+
+function ImgStack(props) {
+  const [displayClass, setDisplayClass] = useState("normalDisplay commonImg");
+  const [hoverClass, setHoverClass] = useState("hoverDisplay commonImg");
+
+  return (
+    <div
+      className="imgStackContainer"
+      onMouseOver={() => {
+        setDisplayClass("hoverDisplay commonImg");
+        setHoverClass("normalDisplay commonImg");
+      }}
+      onMouseOut={() => {
+        setHoverClass("hoverDisplay commonImg");
+        setDisplayClass("normalDisplay commonImg");
+      }}
+    >
+      <img
+        src={props.normalDisplay}
+        alt="normal Image"
+        className={displayClass}
+      />
+      <img
+        src={props.hoverDisplay}
+        alt="hover display"
+        className={hoverClass}
+      />
+    </div>
+  );
+}
+
+export default ImgStack;
