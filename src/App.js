24dd46772e1b8d7ac3c137ba8c24ca804916af34
Akash Gupta <akash.gupta.anup@gmail.com>
2021-07-23 23:24:54 +0530

    added the routes in the app page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Landing from "./components/Landing";
+import SignUp from "./components/SignUp";
 
 function App() {
   return (
-    <div className="App">
-      <Landing />
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path="/" component={Landing} />
+        <Route exact path="/signup" component={SignUp} />
+      </Switch>
+    </Router>
   );
 }
 
