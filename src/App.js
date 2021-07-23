161ede3d4b0aa7de596af68967afac3ec2c8f6f8
Akash Gupta <akash.gupta.anup@gmail.com>
2021-07-23 12:43:50 +0530

    Added the attributes for Main bottom container

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import poster2 from "./assets/landing_page/container_two/container_two.svg";
 import poster3 from "./assets/landing_page/container_three/container_three.svg";
 import poster4 from "./assets/landing_page/container_four/container_four.svg";
 import poster5 from "./assets/landing_page/bottom_community/bottom_community_2.svg";
+import poster6 from "./assets/landing_page/bottom_community/bottom_community.svg";
 
 function App() {
   return (
@@ -48,6 +49,17 @@ function App() {
         Background_color="#FFFFFF"
         isButton
         photo={poster5}
+        photoWidth="47vw"
+        BottomParaContent={data_array[5].description}
+        BottomContainerPadding="3vw"
+      />
+      <MainOuterBottomContainer
+        Background_color="#FEFFE4"
+        photo={poster6}
+        photoWidth="55vw"
+        BottomParaContent={data_array[6].description}
+        BottomContainerPadding="10vw"
+        isLogo
       />
     </div>
   );
