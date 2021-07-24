73d442f3b0438a972234005af44dbf4cff287aca
HarshilMendpara <mendparaharshil@gmail.com>
2021-07-24 14:53:12 +0530

    added some validation in signup form

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 import FormInput from "./signUpCompontents/FormInput";
 import "../styles/Signup/SignUp.css";
+import emailValidator from "email-validator";
+import { passwordValidate } from "../helper/password_validator";
 
 function SignUp() {
   const [userRegistration, setUserRegistration] = useState({
@@ -18,41 +20,70 @@ function SignUp() {
 
   const handleSubmit = (e) => {
     e.preventDefault();
+
+    let name = userRegistration.name;
+    let email = userRegistration.email;
+    let password = userRegistration.password;
+    let confirmPassword = userRegistration.confirmPassword;
+
+    if (!name || !email || !password || !confirmPassword) {
+      return alert('Please fill all details properly!');
+    }
+
+    if (password !== confirmPassword) {
+      return alert('Password not matched!');
+    }
+
+    let emailValidation = emailValidator.validate(email);
+
+    if (!emailValidation) {
+      return alert('Enter a valid email');
+    }
+
+    let passwordValidation = passwordValidate(password);
+
+    if (!passwordValidation[0]) {
+      return alert(`${passwordValidation[1]}`);
+    }
+
     console.log(userRegistration);
   };
 
   return (
-    <form action="" onSubmit={handleSubmit}>
-      <FormInput
-        inputType="text"
-        inputName="name"
-        inputValue={userRegistration.name}
-        lableContent="Name"
-        onChangeFunction={handleInput}
-      />
-      <FormInput
-        inputType="email"
-        inputName="email"
-        inputValue={userRegistration.email}
-        lableContent="Email"
-        onChangeFunction={handleInput}
-      />
-      <FormInput
-        inputType="password"
-        inputName="password"
-        inputValue={userRegistration.password}
-        lableContent="Password"
-        onChangeFunction={handleInput}
-      />
-      <FormInput
-        inputType="password"
-        inputName="confirmPassword"
-        inputValue={userRegistration.confirmPassword}
-        lableContent="Confirm Password"
-        onChangeFunction={handleInput}
-      />
-      <button type="submit">submit</button>
-    </form>
+    <div className="SignUpMainPage">
+      <form action="" onSubmit={handleSubmit} className="SignupPageForm">
+        <p>Sign Up</p>
+        <FormInput
+          inputType="text"
+          inputName="name"
+          inputValue={userRegistration.name}
+          lableContent="Name"
+          onChangeFunction={handleInput}
+        />
+        <FormInput
+          inputType="email"
+          inputName="email"
+          inputValue={userRegistration.email}
+          lableContent="Email"
+          onChangeFunction={handleInput}
+        />
+        <FormInput
+          inputType="password"
+          inputName="password"
+          inputValue={userRegistration.password}
+          lableContent="Password"
+          onChangeFunction={handleInput}
+        />
+        <FormInput
+          inputType="password"
+          inputName="confirmPassword"
+          inputValue={userRegistration.confirmPassword}
+          lableContent="Confirm Password"
+          onChangeFunction={handleInput}
+        />
+        <button type="submit">submit</button>
+      </form>
+    </div>
   );
 }
 
