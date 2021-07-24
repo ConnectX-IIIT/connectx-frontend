4c8db5dcb7dbff8544eab81f5e295905782b24db
Akash Gupta <akash.gupta.anup@gmail.com>
2021-07-24 20:28:06 +0530

    added some changes for the floating label

--- src/components/signUpCompontents/FormInput.js
+++ src/components/signUpCompontents/FormInput.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "../../styles/Signup/FormInput.css";
 
 function FormInput({
@@ -8,21 +8,54 @@ function FormInput({
   lableContent,
   onChangeFunction,
 }) {
+  const [isActive, setActive] = useState(false);
+
+  function handleTextChange(e) {
+    onChangeFunction(e);
+
+    if (e.target.value != "") {
+      setActive(true);
+    } else {
+      setActive(false);
+    }
+  }
+
   return (
-    <div>
-      <label htmlFor={inputName} className="FormLabel">
-        {lableContent}
-      </label>
+    <div className="FormInputDiv">
       <input
         type={inputType}
         name={inputName}
         id={inputName}
         value={inputValue}
-        onChange={onChangeFunction}
+        onChange={handleTextChange}
         className="FormInput"
+        autoComplete="Off"
       />
+      <label
+        htmlFor={inputName}
+        className="FormLabel"
+        style={
+          isActive
+            ? {
+                transform: "translateY(-72%)",
+                fontSize: "0.9vw",
+                transition:
+                  "transform 0.15s ease-out , font-size 0.2s ease-out",
+                paddingLeft: "0.5vw",
+                paddingRight: "0.5vw",
+                left: "12%",
+              }
+            : {
+                transform: "translateY(0)",
+                fontSize: "1.25vw",
+                transition:
+                  "transform 0.15s ease-out , font-size 0.2s ease-out",
+              }
+        }
+      >
+        {lableContent}
+      </label>
     </div>
   );
 }
-
 export default FormInput;
