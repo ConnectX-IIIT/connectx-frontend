99ca562256c54ebe98710756e3fec3de1cee71cc
HarshilMendpara <mendparaharshil@gmail.com>
2021-07-25 13:55:17 +0530

    added google login function

--- src/components/signUpCompontents/SignUpFormBottom.js
+++ src/components/signUpCompontents/SignUpFormBottom.js
@@ -1,19 +1,43 @@
+import Cookies from "js-cookie";
 import React from "react";
+import GoogleLogin from "react-google-login";
 import { Link } from "react-router-dom";
-import GoogleIcon from "../../assets/signinup_page/ic_google.svg";
+import instance from "../../helper/axios";
+// import GoogleIcon from "../../assets/signinup_page/ic_google.svg";
 import "../../styles/Signup/SignUpFormBottom.css";
 
 function SignUpFormBottom() {
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
+  const responseSuccessGoogle = async (response) => {
+    try {
+      const googleLoginRes = await instance.post(`/auth/googlelogin`,
+        { tokenId: response.tokenId }
+      )
+      const googleLoginData = googleLoginRes.data;
+
+      Cookies.set("token", googleLoginData.token, { expires: 1, secure: true });
+
+    } catch (error) {
+      return alert(`Server error occured!`);
+    }
+  }
+
+  const responseErrorGoogle = (response) => {
+    alert(`Google login error!`);
   }
 
   return (
     <div className="SignUpformBottom">
       <p id="SignUpformBottomPara"> Or continue with</p>
       <div className="SignUpBottomImageContainer">
-        <img onClick={handleSubmit} src={GoogleIcon} alt="Google" />
+        {/* <img onClick={handleSubmit} src={GoogleIcon} alt="Google" /> */}
+        <GoogleLogin
+          clientId="983656845468-hlvmorv2emsk7l94rmmfi98i4qs0828p.apps.googleusercontent.com"
+          buttonText="Login with google"
+          onSuccess={responseSuccessGoogle}
+          onFailure={responseErrorGoogle}
+          cookiePolicy={'single_host_origin'}
+        />
       </div>
       <Link to="/" id="SignUpformBottomAnchor">Forgot Password?</Link>
     </div>
