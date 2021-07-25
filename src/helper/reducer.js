b17975ae12b86ce76c1f1180c88ae1bdd2af799c
HarshilMendpara <mendparaharshil@gmail.com>
2021-07-25 13:54:50 +0530

    added reducer and state provider files

---
+++ src/helper/reducer.js
@@ -0,0 +1,15 @@
+export const initialState = {
+    userDetails: {
+        "name": "",
+        "email": "",
+        "mobile": "",
+        "joiningYear": "",
+        "passingYear": "",
+        "gender": "",
+        "id": "",
+        "isAlumni": "",
+        "isMailVerified": "",
+        "isVerified": "",
+        "batch": ""
+    }
+};
