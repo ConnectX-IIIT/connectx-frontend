9a12ff0ff63f14a0896597b053f9c06034a57d52
Akash Gupta <akash.gupta.anup@gmail.com>
2021-07-25 12:04:02 +0530

    formating of axios

--- src/helper/axios.js
+++ src/helper/axios.js
@@ -1,7 +1,7 @@
-import axios from 'axios';
+import axios from "axios";
 
 const instance = axios.create({
-    baseURL: 'https://obscure-ridge-13663.herokuapp.com'
-})
+  baseURL: "https://obscure-ridge-13663.herokuapp.com",
+});
 
 export default instance;
