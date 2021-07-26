df601f07b77b5d80ffc0d0fd160598ee7d7a31a1
Akash Gupta <akash.gupta.anup@gmail.com>
2021-07-26 15:27:38 +0530

    Created the About js page

--- src/components/About.js
+++ src/components/About.js
@@ -1,15 +1,38 @@
-import React from 'react'
-import Start from "./About_Components/Start"
-import Middle from "./About_Components/Middle"
+import React from "react";
+import TopAboutContainer from "./About_Components/TopAboutContainer";
+import BottomAboutContainer from "./About_Components/BottomAboutContainer";
+import "../styles/About/About.css";
+import AboutUsGroup from "../assets/about_us/about_us_one.svg";
+import AboutUsShadow from "../assets/about_us/about_us_one_bottom_shadow_1.svg";
+import AboutUsShadow2 from "../assets/about_us/about_us_one_bottom_shadow_2.svg";
 
-function About () {
-    return (
-        <div>
-         <Start />
-
-         <Middle />
-        </div>
-    )
+function About() {
+  return (
+    <div
+      style={{
+        position: "relative",
+      }}
+    >
+      <TopAboutContainer />
+      <div
+        className="AboutImageContainer"
+        style={{
+          position: "absolute",
+          top: "24%",
+          left: "27%",
+        }}
+      >
+        <img src={AboutUsGroup} className="AboutUsGroup" alt="Aboutus" />
+        <img src={AboutUsShadow} className="AboutUsShadow" alt="Aboutshadow" />
+        <img
+          src={AboutUsShadow2}
+          className="AboutUsShadow2"
+          alt="Aboutshadow"
+        />
+      </div>
+      <BottomAboutContainer />
+    </div>
+  );
 }
 
 export default About;
