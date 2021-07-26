9c29af0ecd7d5c0e880dfdad9086536390f3f463
Akash Gupta <akash.gupta.anup@gmail.com>
2021-07-26 19:18:28 +0530

    added the image styling and footer components

--- src/components/About.js
+++ src/components/About.js
@@ -1,10 +1,13 @@
 import React from "react";
 import TopAboutContainer from "./About_Components/TopAboutContainer";
 import BottomAboutContainer from "./About_Components/BottomAboutContainer";
+import FooterAboutPage from "./About_Components/FooterAboutPage";
+
 import "../styles/About/About.css";
 import AboutUsGroup from "../assets/about_us/about_us_one.svg";
 import AboutUsShadow from "../assets/about_us/about_us_one_bottom_shadow_1.svg";
 import AboutUsShadow2 from "../assets/about_us/about_us_one_bottom_shadow_2.svg";
+import ConnectxLogo from "../assets/_logo/svg/logo.svg";
 
 function About() {
   return (
@@ -13,6 +16,17 @@ function About() {
         position: "relative",
       }}
     >
+      <img
+        src={ConnectxLogo}
+        alt="connectxlogo"
+        style={{
+          position: "absolute",
+          top: "3.32vw",
+          left: "44.15vw",
+          width: "10.67vw",
+          objectFit: "contain",
+        }}
+      />
       <TopAboutContainer />
       <div
         className="AboutImageContainer"
@@ -20,6 +34,8 @@ function About() {
           position: "absolute",
           top: "24%",
           left: "27%",
+          height: "23.1vw",
+          width: "46.2vw",
         }}
       >
         <img src={AboutUsGroup} className="AboutUsGroup" alt="Aboutus" />
@@ -31,6 +47,7 @@ function About() {
         />
       </div>
       <BottomAboutContainer />
+      <FooterAboutPage />
     </div>
   );
 }
