528db11522ee5992029a6ff283ccc8bd8ab69a21
Akash Gupta <akash.gupta.anup@gmail.com>
2021-07-26 15:28:43 +0530

    Create BottomAboutContainer.js

---
+++ src/components/About_Components/BottomAboutContainer.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "../../styles/About/BottomAboutContainer.css";
+
+function BottomAboutContainer() {
+  return <div className="BottomAboutContainer"></div>;
+}
+
+export default BottomAboutContainer;
