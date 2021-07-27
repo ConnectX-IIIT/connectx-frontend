27c7de3a092499cd10f1d605fc2b74536c040781
Akash Gupta <akash.gupta.anup@gmail.com>
2021-07-27 19:41:57 +0530

    added addphoto route

--- src/App.js
+++ src/App.js
@@ -6,35 +6,33 @@ import SignIn from "./components/SignIn";
 import Register from "./components/Register";
 import About from "./components/About";
 import Home from "./components/Home";
+import PhotoUpload from "./components/PhotoUpload";
 import Cookies from "js-cookie";
 import instance from "./helper/axios";
 import { useStateValue } from "./helper/state_provider";
 
 function App() {
-
   const [{ userDetails }, dispatch] = useStateValue();
 
   useEffect(() => {
     async function fetchData() {
-
       try {
         const token = Cookies.get("token");
 
         if (token) {
           const getDetailsRes = await instance.get(`/user/getdetails`, {
             headers: {
-              "Authorization": `${token}`
-            }
-          })
+              Authorization: `${token}`,
+            },
+          });
 
           const userData = getDetailsRes.data.userData;
 
           dispatch({
-            type: 'UPDATE_DETAILS',
-            userData: userData
-          })
+            type: "UPDATE_DETAILS",
+            userData: userData,
+          });
         }
-
       } catch (error) {
         return alert(`${error.response.data.error}`);
       }
@@ -51,6 +49,7 @@ function App() {
         <Route exact path="/register" component={Register} />
         <Route exact path="/about" component={About} />
         <Route exact path="/home" component={Home} />
+        <Route exact path="/photoupload" component={PhotoUpload} />
       </Switch>
     </Router>
   );
