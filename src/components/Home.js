7b771eebca0b7d19f2a8759403c84421273f12b3
Akash Gupta <akash.gupta.anup@gmail.com>
2021-07-29 13:24:58 +0530

    added the homepage css

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,7 @@
-import React from 'react'
+import React from "react";
+import "../styles/HomePage/HomePage.css";
 
 export const Home = () => {
-    return (
-        <div>
-            hello
-        </div>
-    )
-}
- export default Home
+  return <div>hello world</div>;
+};
+export default Home;
