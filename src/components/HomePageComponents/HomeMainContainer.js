11a8fb85a059ad6132816f9347b2148d406dc718
Akash Gupta <akash.gupta.anup@gmail.com>
2021-07-31 16:00:11 +0530

    created and added the homepage card

--- src/components/HomePageComponents/HomeMainContainer.js
+++ src/components/HomePageComponents/HomeMainContainer.js
@@ -1,11 +1,13 @@
 import React from "react";
 import HomeUserDetails from "./HomeUserDetails";
+import HomePageCard from "./HomePageCard";
 import "../../styles/HomePage/HomeMainContainer/HomeMainContainer.css";
 
 function HomeMainContainer() {
   return (
     <div className="HomeMainContainer">
       <HomeUserDetails />
+      <HomePageCard />
     </div>
   );
 }
