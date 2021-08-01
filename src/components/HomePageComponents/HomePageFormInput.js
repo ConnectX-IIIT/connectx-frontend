a9ddba10fcdd4b21a92ace2a233262a44077da8e
Akash Gupta <akash.gupta.anup@gmail.com>
2021-08-01 22:39:29 +0530

    HomePageFormInput with placeholder

---
+++ src/components/HomePageComponents/HomePageFormInput.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+import "../../styles/HomePage/HomeMainContainer/HomePageFormInput.css";
+
+function HomePageFormInput({ inputValue, onChangeFunction }) {
+  return (
+    <div className="FormInputDiv">
+      <input
+        type="text"
+        name="searchedText"
+        id="searchedText"
+        value={inputValue}
+        onChange={onChangeFunction}
+        className="FormInput"
+        placeholder="Search Something"
+        style={{
+          marginBottom: "0",
+          marginLeft: "2vw",
+          width: "24.9vw",
+        }}
+      />
+    </div>
+  );
+}
+
+export default HomePageFormInput;
