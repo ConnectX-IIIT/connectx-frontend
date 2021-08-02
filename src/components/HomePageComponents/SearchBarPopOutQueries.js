e2478ede4cfc240f81c49a8515313be1d3da3bc7
Akash Gupta <akash.gupta.anup@gmail.com>
2021-08-02 22:16:27 +0530

    added the searchbarpopoutqueries

---
+++ src/components/HomePageComponents/SearchBarPopOutQueries.js
@@ -0,0 +1,32 @@
+import React from "react";
+
+function SearchBarPopOutQueries() {
+  return (
+    <div
+      style={{
+        display: "flex",
+        alignItems: "center",
+        paddingLeft: "1vw",
+        paddingTop: "0.5vw",
+        paddingBottom: "0.5vw",
+        borderBottom: "1px solid #959595",
+        cursor: "pointer",
+      }}
+    >
+      <div
+        style={{
+          fontFamily: "'Manrope' , sans-serif",
+          fontSize: "0.9vw",
+          fontWeight: "600",
+          fontStyle: "normal",
+          color: "#494949",
+          marginRight: "0.5vw",
+        }}
+      >
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum,
+      </div>
+    </div>
+  );
+}
+
+export default SearchBarPopOutQueries;
