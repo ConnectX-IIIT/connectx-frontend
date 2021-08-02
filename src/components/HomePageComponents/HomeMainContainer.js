eaf666d9813d0e31f98d31d8e49a5825b051d292
Akash Gupta <akash.gupta.anup@gmail.com>
2021-08-02 15:10:41 +0530

    Added the props for the Homepage >> Props added are : UserprofilePhoto Timestamp PostUserName PostContent Image url upvotes

--- src/components/HomePageComponents/HomeMainContainer.js
+++ src/components/HomePageComponents/HomeMainContainer.js
@@ -3,16 +3,60 @@ import HomeUserDetails from "./HomeUserDetails";
 import HomePageCard from "./HomePageCard";
 import "../../styles/HomePage/HomeMainContainer/HomeMainContainer.css";
 
+const HomePageCardDetails = [
+  {
+    PostId: "60f68ade7e4fdb95c36eb07f",
+    UserId: "60f68ade7e4fdb95c36eb07f",
+    UserProfilePhoto: `
+    https://obscure-ridge-13663.herokuapp.com/user/fetch/30a22c6882f8cb81415b5cf75a86c056`,
+    TimeStamp: "1626768841451",
+    PostUserName: "Raj Noobda",
+    PostContent: `Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.
+
+    The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.`,
+    PostImageUrls: [
+      "https://obscure-ridge-13663.herokuapp.com/user/fetch/cf13802eb3fa394a011085ce93a77003",
+      "https://obscure-ridge-13663.herokuapp.com/user/fetch/cf13802eb3fa394a011085ce93a77003",
+    ],
+    Upvotes: 19,
+  },
+  {
+    PostId: "60f68ade7e4fdb95c36eb07f",
+    UserId: "60f68ade7e4fdb95c36eb07f",
+    UserProfilePhoto: `
+    https://obscure-ridge-13663.herokuapp.com/user/fetch/30a22c6882f8cb81415b5cf75a86c056`,
+    TimeStamp: "1626768841451",
+    PostUserName: "Raj Noobda",
+    PostContent: `Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32 . \n
+    The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.`,
+    PostImageUrls: [
+      "https://obscure-ridge-13663.herokuapp.com/user/fetch/cf13802eb3fa394a011085ce93a77003",
+      "https://obscure-ridge-13663.herokuapp.com/user/fetch/cf13802eb3fa394a011085ce93a77003",
+    ],
+    Upvotes: 19,
+  },
+];
+
 function HomeMainContainer() {
   return (
     <div className="HomeMainContainer">
       <HomeUserDetails />
       <div>
-        <HomePageCard />
-        <HomePageCard />
+        <HomePageCard
+          UserProfilePhoto={HomePageCardDetails[0].UserProfilePhoto}
+          TimeStamp={HomePageCardDetails[0].TimeStamp}
+          PostUserName={HomePageCardDetails[0].PostUserName}
+          PostContent={HomePageCardDetails[0].PostContent}
+          PostImageUrls={HomePageCardDetails[0].PostImageUrls}
+          Upvotes={HomePageCardDetails[0].Upvotes}
+        />
       </div>
     </div>
   );
 }
 
 export default HomeMainContainer;
+// UserProfilePhoto
+// TimeStamp
+// Post COntent
+// Post Image url
