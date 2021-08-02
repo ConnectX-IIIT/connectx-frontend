b137561a1fb3a94cb209afb3e8f50b79b3f690cf
Akash Gupta <akash.gupta.anup@gmail.com>
2021-08-02 23:05:49 +0530

    changed the link

--- src/components/HomePageComponents/HomeMainContainer.js
+++ src/components/HomePageComponents/HomeMainContainer.js
@@ -8,7 +8,7 @@ const HomePageCardDetails = [
     PostId: "60f68ade7e4fdb95c36eb07f",
     UserId: "60f68ade7e4fdb95c36eb07f",
     UserProfilePhoto: `
-    https://obscure-ridge-13663.herokuapp.com/user/fetch/30a22c6882f8cb81415b5cf75a86c056`,
+    https://obscure-ridge-13663.herokuapp.com/user/fetch/cf13802eb3fa394a011085ce93a77003`,
     TimeStamp: "1626768841451",
     PostUserName: "Raj Noobda",
     PostContent: `Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.
@@ -24,7 +24,7 @@ const HomePageCardDetails = [
     PostId: "60f68ade7e4fdb95c36eb07f",
     UserId: "60f68ade7e4fdb95c36eb07f",
     UserProfilePhoto: `
-    https://obscure-ridge-13663.herokuapp.com/user/fetch/30a22c6882f8cb81415b5cf75a86c056`,
+    https://obscure-ridge-13663.herokuapp.com/user/fetch/cf13802eb3fa394a011085ce93a77003`,
     TimeStamp: "1626768841451",
     PostUserName: "Raj Noobda",
     PostContent: `Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32 .
