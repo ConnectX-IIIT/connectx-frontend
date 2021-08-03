d99468582b5074a7e78c908041b4fcef096c11c5
HarshilMendpara <mendparaharshil@gmail.com>
2021-08-03 17:18:31 +0530

    fetched postData from db and displayed posts

--- src/components/HomePageComponents/HomeMainContainer.js
+++ src/components/HomePageComponents/HomeMainContainer.js
@@ -1,59 +1,54 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import HomeUserDetails from "./HomeUserDetails";
 import HomePageCard from "./HomePageCard";
 import "../../styles/HomePage/HomeMainContainer/HomeMainContainer.css";
-
-const HomePageCardDetails = [
-  {
-    PostId: "60f68ade7e4fdb95c36eb07f",
-    UserId: "60f68ade7e4fdb95c36eb07f",
-    UserProfilePhoto: `
-    https://obscure-ridge-13663.herokuapp.com/user/fetch/01809a6ff6e4ea2ce439f7c1bb21d38e`,
-    TimeStamp: "1626768841451",
-    PostUserName: "Raj Noobda",
-    PostContent: `Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.
-
-    The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.`,
-    PostImageUrls: [
-      "https://obscure-ridge-13663.herokuapp.com/user/fetch/11f59e332b95986deb8682c8224547f2",
-      "https://obscure-ridge-13663.herokuapp.com/user/fetch/11f59e332b95986deb8682c8224547f2",
-    ],
-    Upvotes: 19,
-  },
-  {
-    PostId: "60f68ade7e4fdb95c36eb07f",
-    UserId: "60f68ade7e4fdb95c36eb07f",
-    UserProfilePhoto: `
-    https://obscure-ridge-13663.herokuapp.com/user/fetch/01809a6ff6e4ea2ce439f7c1bb21d38e`,
-    TimeStamp: "1626768841451",
-    PostUserName: "Raj Noobda",
-    PostContent: `Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32 .
-    The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.`,
-    PostImageUrls: [
-      "https://obscure-ridge-13663.herokuapp.com/user/fetch/11f59e332b95986deb8682c8224547f2",
-      "https://obscure-ridge-13663.herokuapp.com/user/fetch/11f59e332b95986deb8682c8224547f2",
-    ],
-    Upvotes: 19,
-  },
-];
-
-const HomePageCardDetailsList = HomePageCardDetails.map((item, index) => {
-  return (
-    <HomePageCard
-      key={index}
-      PostId={item.PostId}
-      UserId={item.UserId}
-      UserProfilePhoto={item.UserProfilePhoto}
-      TimeStamp={item.TimeStamp}
-      PostUserName={item.PostUserName}
-      PostContent={item.PostContent}
-      PostImageUrls={item.PostImageUrls}
-      Upvotes={item.Upvotes}
-    />
-  );
-});
+import instance from "../../helper/axios";
+import Cookies from "js-cookie";
 
 function HomeMainContainer() {
+
+  const [postData, setPostData] = useState([]);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async (e) => {
+    try {
+      const token = Cookies.get("token");
+
+      if (token) {
+        const getDetailsRes = await instance.get(`/home/posts`, {
+          headers: {
+            Authorization: `${token}`,
+          },
+        });
+
+        const Data = getDetailsRes.data.postData;
+        setPostData(Data);
+
+      }
+    } catch (error) {
+      return alert(`${error.response.data.error}`);
+    }
+  }
+
+  const HomePageCardDetailsList = postData.map((item, index) => {
+    return (
+      <HomePageCard
+        key={index}
+        PostId={item._id}
+        UserId={item.user}
+        UserProfilePhoto={item.userProfile}
+        TimeStamp={item.timestamp}
+        PostUserName={item.userName}
+        PostContent={item.description}
+        PostImageUrls={item.attachedImages}
+        Upvotes={item.reactions}
+      />
+    );
+  });
+
   return (
     <div className="HomeMainContainer">
       <HomeUserDetails />
