97a995754fa675522b4f3a90e98cdf2550c57ca2
HarshilMendpara <mendparaharshil@gmail.com>
2021-08-03 18:06:10 +0530

    converted timestamp in human time

--- src/components/HomePageComponents/HomePageCard.js
+++ src/components/HomePageComponents/HomePageCard.js
@@ -38,6 +38,23 @@ function HomePageCard({
     return UserProfileDefaultIcon;
   }
 
+  const handleTimestamp = (timestamp) => {
+    let time = new Date(timestamp);
+    let today = new Date();
+    let yesterday = new Date((new Date()).valueOf() - 1000 * 60 * 60 * 24);
+    let timeString;
+
+    if (today.getDate() === time.getDate() && Date.now() - timestamp < 86400000) {
+      timeString = "Today";
+    } else if (yesterday.getDate() === time.getDate() && Date.now() - timestamp < 172800000) {
+      timeString = "Yesterday";
+    } else {
+      timeString = time.getDate() + " " + time.toLocaleString('default', { month: 'short' }) + " " + time.getFullYear();
+    }
+
+    return timeString + " " + time.toLocaleString('en-US', { hour: 'numeric', minute: 'numeric', hour12: true });
+  }
+
   return (
     <div className="HomePageCard">
       <div id="HomePageCardLeftContainer">
@@ -107,7 +124,7 @@ function HomePageCard({
                   fontSize: "0.9vw",
                 }}
               >
-                {TimeStamp}
+                {handleTimestamp(TimeStamp)}
               </div>
             </div>
             <img
