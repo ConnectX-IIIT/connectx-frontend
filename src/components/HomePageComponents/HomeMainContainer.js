c50c9bf29bc943ae9ccaa9878a778741af2412b3
HarshilMendpara <mendparaharshil@gmail.com>
2021-08-04 15:40:55 +0530

    redirected to signin on session expire

--- src/components/HomePageComponents/HomeMainContainer.js
+++ src/components/HomePageComponents/HomeMainContainer.js
@@ -4,33 +4,36 @@ import HomePageCard from "./HomePageCard";
 import "../../styles/HomePage/HomeMainContainer/HomeMainContainer.css";
 import instance from "../../helper/axios";
 import Cookies from "js-cookie";
+import { useHistory } from "react-router-dom";
 
 function HomeMainContainer() {
 
+  const history = useHistory();
   const [postData, setPostData] = useState([]);
 
-  useEffect(() => {
-    const fetchData = async (e) => {
-      try {
-        const token = Cookies.get("token");
+  const fetchData = async (e) => {
+    try {
+      const token = Cookies.get("token");
 
-        if (token) {
-          const getDetailsRes = await instance.get(`/home/posts`, {
-            headers: {
-              Authorization: `${token}`,
-            },
-          });
+      if (token) {
+        const getDetailsRes = await instance.get(`/home/posts`, {
+          headers: {
+            Authorization: `${token}`,
+          },
+        });
 
-          const data = getDetailsRes.data.postData;
-          setPostData(data);
+        const data = getDetailsRes.data.postData;
+        setPostData(data);
 
-        }
-      } catch (error) {
-        return alert(`${error.response.data.error}`);
+      } else {
+        history.replace('/signin')
       }
+    } catch (error) {
+      return alert(`${error.response.data.error}`);
     }
-    fetchData();
-  }, []);
+  }
+
+  useEffect(() => { fetchData() }, []);
 
   const HomePageCardDetailsList = postData.map((item, index) => {
     return (
