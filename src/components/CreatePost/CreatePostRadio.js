acd63eadd63a4c0b6bed744e5b993bb4e73111c2
Akash Gupta <akash.gupta.anup@gmail.com>
2021-08-07 14:49:50 +0530

    created the createpostradio from the input of register

---
+++ src/components/CreatePost/CreatePostRadio.js
@@ -0,0 +1,62 @@
+import React from "react";
+
+function CreatePostRadio({
+  RadioHeading,
+  RadioOptions,
+  RadioName,
+  onChangeFunction,
+  RadioValue,
+}) {
+  const RadioOptionsList = RadioOptions.map((RadioOption) => {
+    return (
+      <>
+        <input
+          type="radio"
+          id={RadioHeading + RadioOption}
+          name={RadioName}
+          onChange={onChangeFunction}
+          value={RadioValue}
+          key={RadioHeading + RadioOption}
+          className="radioInput"
+        />
+        <label
+          htmlFor={RadioHeading + RadioOption}
+          key={RadioOption}
+          className="radioLabel"
+          style={{
+            color: "#717171",
+          }}
+        >
+          {RadioOption}
+        </label>
+      </>
+    );
+  });
+
+  return (
+    <div
+      style={{
+        display: "inline-block",
+        marginLeft: "4vw",
+      }}
+    >
+      <p
+        style={{
+          fontFamily: "'Manrope' , sans-serif",
+          fontStyle: "normal",
+          fontWeight: "500",
+          fontSize: "1vw",
+          lineHeight: "1.4vw",
+          color: "#A6A6A6",
+          marginBottom: "0.7vw",
+        }}
+      >
+        {RadioHeading}
+      </p>
+
+      {RadioOptionsList}
+    </div>
+  );
+}
+
+export default CreatePostRadio;
