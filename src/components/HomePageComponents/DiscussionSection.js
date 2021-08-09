c3debe8ead0c1b624a9eacc77adf46024ec7b30e
Akash Gupta <akash.gupta.anup@gmail.com>
2021-08-09 21:56:45 +0530

    Added the discussion styles and changed the image width for the upvote and downvote section

--- src/components/HomePageComponents/DiscussionSection.js
+++ src/components/HomePageComponents/DiscussionSection.js
@@ -12,9 +12,9 @@ import homeDownvoteIconSelected from "../../assets/home/post/upvotes/s_ic_downvo
 import UserProfile from "../../assets/profile/user_profile_default_icon.svg";
 import HomeCardInnerContent from "./HomeCardInnerContent";
 
-function DiscussionSection() {
-  const InnerContent = `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Facilisis aliquam bibendum turpis faucibus duis at aliquam. Sed eu turpis nisl morbi vel luctus volutpat turpis. Facilisis velit in rutrum neque, 
-
+function DiscussionSection({ discussionSectionStyle }) {
+  const InnerContent = `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Facilisis aliquam bibendum turpis faucibus duis at aliquam. Sed eu turpis nisl morbi vel luctus volutpat turpis. Facilisis velit in rutrum neque,
+   
 sed quis et. Sit pretium nunc arcu enim cras turpis donec tristique. Vulputate rhoncus duis id nulla posuere commodo lacus arcu. Elementum aenean ac scelerisque morbi. Varius odio commodo diam fusce leo. Scelerisque bibendum accumsan, integer orci, massa, ornare mauris id. Diam egestas ege`;
 
   const [UpvotesHandle, setUpvotesHandle] = useState(0);
@@ -53,72 +53,75 @@ sed quis et. Sit pretium nunc arcu enim cras turpis donec tristique. Vulputate r
     }
   };
   return (
-    <div>
-      <div className="flex">
-        <img
-          src={UserProfile}
-          alt="userprofile"
-          className="object-cover w-10 h-10 mx-6"
-        />
-        <div className="HomePageCard ml-0 mr-2">
-          <div id="HomePageCardLeftContainer">
-            <ImgStackHome
-              normalImageSrc={homeUpvoteIcon}
-              hoverImageSrc={homeUpvoteIconHover}
-              activeImageSrc={homeUpvoteIconSelected}
-              styleImgStack={{
-                transitionDuration: "0.2s",
-                width: "2vw",
-                height: "2vw",
-              }}
-              styleImgContainer={{ margin: "0", width: "2vw", height: "2vw" }}
-              onClickFunction={() => {
-                handleReaction(true);
-              }}
-              isActive={UpvoteActive}
-            />
-            <div id="HomePageCardLeftContainerCount">{UpvotesHandle}</div>
-            <ImgStackHome
-              normalImageSrc={homeDownvoteIcon}
-              hoverImageSrc={homeDownvoteIconHover}
-              activeImageSrc={homeDownvoteIconSelected}
-              styleImgStack={{
-                transitionDuration: "0.2s",
-                width: "2vw",
-                height: "2vw",
-              }}
-              styleImgContainer={{ margin: "0", width: "2vw", height: "2vw" }}
-              onClickFunction={() => {
-                handleReaction(false);
-              }}
-              isActive={DownvoteActive}
-            />
-          </div>
-          <div
-            id="HomePageCardRightContainer"
-            style={{
-              paddingLeft: "1.5vw",
-              paddingRight: "1.5vw",
+    <div className="flex pt-4" style={discussionSectionStyle}>
+      <img
+        src={UserProfile}
+        alt="userprofile"
+        className="object-cover w-10 h-10 mx-5"
+      />
+      <div className="HomePageCard ml-0 mr-2">
+        <div id="HomePageCardLeftContainer">
+          <ImgStackHome
+            normalImageSrc={homeUpvoteIcon}
+            hoverImageSrc={homeUpvoteIconHover}
+            activeImageSrc={homeUpvoteIconSelected}
+            styleImgStack={{
+              transitionDuration: "0.2s",
+              width: "2vw",
+              height: "2vw",
             }}
+            styleImgContainer={{ margin: "0", width: "1.5vw", height: "2vw" }}
+            onClickFunction={() => {
+              handleReaction(true);
+            }}
+            isActive={UpvoteActive}
+          />
+          <div
+            id="HomePageCardLeftContainerCount"
+            className="text-base leading-3"
           >
-            <div className="mt-4 mb-2">
-              <h2 className="font-manrope font-medium inline text-lg mr-3">
-                Raj Noobda
-              </h2>
-              <p
-                className="font-manrope inline text-xs font-semibold"
-                style={{
-                  color: "#999999",
-                }}
-              >
-                15:36
-              </p>
-            </div>
-            <HomeCardInnerContent
-              InnerContent={InnerContent}
-              styleInnerContent={{ fontSize: "1rem" }}
-            />
+            {UpvotesHandle}
           </div>
+          <ImgStackHome
+            normalImageSrc={homeDownvoteIcon}
+            hoverImageSrc={homeDownvoteIconHover}
+            activeImageSrc={homeDownvoteIconSelected}
+            styleImgStack={{
+              transitionDuration: "0.2s",
+              width: "2vw",
+              height: "2vw",
+            }}
+            styleImgContainer={{ margin: "0", width: "1.5vw", height: "2vw" }}
+            onClickFunction={() => {
+              handleReaction(false);
+            }}
+            isActive={DownvoteActive}
+          />
+        </div>
+        <div
+          id="HomePageCardRightContainer"
+          style={{
+            paddingLeft: "1vw",
+            paddingRight: "1vw",
+          }}
+        >
+          <div className="mt-4 mb-2">
+            <h2 className="font-manrope font-medium inline text-lg mr-3">
+              Raj Noobda
+            </h2>
+            <p
+              className="font-manrope inline text-xs font-semibold"
+              style={{
+                color: "#999999",
+              }}
+            >
+              15:36
+            </p>
+          </div>
+          <HomeCardInnerContent
+            InnerContent={InnerContent}
+            styleInnerContent={{ fontSize: "1rem" }}
+          />
         </div>
       </div>
     </div>
