f2b59c4ed541fc4fe35c60dfd9c467062a1406d7
Akash Gupta <akash.gupta.anup@gmail.com>
2021-08-14 14:23:58 +0530

    Added the SortComponent and HomePageCard component

--- src/components/Queries/QueriesMainConatiner.js
+++ src/components/Queries/QueriesMainConatiner.js
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 import CreatePostInput from "./../CreatePost/CreatePostInput";
 import ProfilePhoto from "../../assets/profile/user_profile_default_icon.svg";
+import SortComponent from "./SortComponent";
+import HomePageCard from "./../HomePageComponents/HomePageCard";
 
 function QueriesMainConatiner() {
   const [UserQueries, setUserQueries] = useState({
@@ -44,6 +46,33 @@ function QueriesMainConatiner() {
           style={{ width: "100%" }}
         />
       </div>
+
+      <SortComponent />
+
+      <HomePageCard
+        UserProfilePhoto={ProfilePhoto}
+        TimeStamp={1223355499}
+        PostUserName="Raj Noobda"
+        PostContent="What Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc non posuere neque, a tincidunt sem?"
+        PostImageUrls={[]}
+        Upvotes="12"
+        PostId="1"
+        isDiscussionQueries={true}
+        queriesInnerStyle={{ fontWeight: "600", fontFamily: "manrope" }}
+        queriesMainContainerStyle={{ marginLeft: "0" }}
+      />
+      <HomePageCard
+        UserProfilePhoto={ProfilePhoto}
+        TimeStamp={1223355499}
+        PostUserName="Raj Noobda"
+        PostContent="What Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc non posuere neque, a tincidunt sem?"
+        PostImageUrls={[]}
+        Upvotes="12"
+        PostId="1"
+        isDiscussionQueries={true}
+        queriesInnerStyle={{ fontWeight: "600", fontFamily: "manrope" }}
+        queriesMainContainerStyle={{ marginLeft: "0" }}
+      />
     </div>
   );
 }
