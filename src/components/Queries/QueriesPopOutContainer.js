a8edcd83d2da44ce187d75ddbb61448b8b8b3a5d
Akash Gupta <akash.gupta.anup@gmail.com>
2021-08-14 17:18:44 +0530

    Created popout for queries section

---
+++ src/components/Queries/QueriesPopOutContainer.js
@@ -0,0 +1,86 @@
+import React, { useState } from "react";
+import CreatePostInput from "../CreatePost/CreatePostInput";
+import deleteIcon from "../../assets/create_post/ic_close.svg";
+
+function QuestionSuggestion({ QuestionInnerContent }) {
+  return (
+    <div
+      className="pl-6 border-2 border-t-0 mx-1.5 font-manrope font-semibold py-1.5 text-lg truncate cursor-pointer"
+      style={{
+        borderColor: "#959595",
+        width: "37.219vw",
+      }}
+    >
+      {QuestionInnerContent}
+    </div>
+  );
+}
+
+function QueriesPopOutContainer() {
+  const [UserQueries, setUserQueries] = useState({
+    askedQuestion: "",
+  });
+
+  const handleInput = (e) => {
+    const name = e.target.name;
+    const value = e.target.value;
+    setUserQueries({ ...UserQueries, [name]: value });
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(UserQueries);
+  };
+
+  return (
+    <div className="PostMainContainer rounded-md">
+      <div className="flex justify-between mb-11">
+        <h2 className="font-manrope font-semibold text-xl">Add Question</h2>
+        <img
+          src={deleteIcon}
+          alt="delete"
+          onClick={() => {
+            // document
+            //   .getElementById("HomeContainerCreatePost")
+            //   .classList.toggle("hidden");
+          }}
+          className="cursor-pointer"
+        />
+      </div>
+
+      <form
+        action=""
+        onSubmit={handleSubmit}
+        encType="multipart/form-data"
+        method="post"
+        className="flex flex-col"
+        style={{
+          width: "38vw",
+        }}
+      >
+        <CreatePostInput
+          inputType="text"
+          inputName="askedQuestion"
+          inputValue={UserQueries.askedQuestion}
+          onChangeFunction={handleInput}
+          labelContent="Ask Something"
+          isInput={true}
+          style={{ width: "100%", marginBottom: "0" }}
+        />
+      </form>
+      <QuestionSuggestion QuestionInnerContent="lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem lorem ipsum  " />
+      <QuestionSuggestion QuestionInnerContent="lorem ipsum" />
+      <QuestionSuggestion QuestionInnerContent="lorem ipsum" />
+
+      <div className="flex">
+        <button
+          className="w-28 rounded h-9 font-manrope font-semibold text-white transition-colors duration-200 hover:bg-blue-500 my-8 mx-auto"
+          style={{ backgroundColor: "#C4C4C4" }}
+        >
+          Post
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default QueriesPopOutContainer;
