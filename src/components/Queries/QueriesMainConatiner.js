bf8a3ec338c6cba1a5548d2e4da61ff25aa8115b
Akash Gupta <akash.gupta.anup@gmail.com>
2021-08-14 15:28:01 +0530

    added popup

--- src/components/Queries/QueriesMainConatiner.js
+++ src/components/Queries/QueriesMainConatiner.js
@@ -3,12 +3,23 @@ import QueriesQuestionContainer from "./QueriesQuestionContainer";
 
 function QueriesMainConatiner() {
   return (
-    <div
-      className="flex flex-col max-w-screen-lg font-manrope mx-auto"
-      style={{ marginTop: "calc(81px + 1.2vw)", width: "50vw" }}
-    >
-      <QueriesQuestionContainer />
-    </div>
+    <>
+      <div
+        className="w-full absolute  left-0 z-10 "
+        id="HomeContainerCreatePost"
+        style={{
+          backgroundColor: "rgb(0, 19, 36 , 0.6)",
+          top: "-1.2vw",
+          height: "calc(100% + 1.2vw)",
+        }}
+      ></div>
+      <div
+        className="flex flex-col max-w-screen-lg font-manrope mx-auto relative"
+        style={{ marginTop: "calc(81px + 1.2vw)", width: "50vw" }}
+      >
+        <QueriesQuestionContainer />
+      </div>
+    </>
   );
 }
 
