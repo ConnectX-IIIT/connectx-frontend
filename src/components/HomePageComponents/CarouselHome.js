8e39a813c914f5125601de9cca9c8ca7465ed7c4
HarshilMendpara <mendparaharshil@gmail.com>
2021-08-15 15:28:05 +0530

    added default icon in carousel image

--- src/components/HomePageComponents/CarouselHome.js
+++ src/components/HomePageComponents/CarouselHome.js
@@ -1,16 +1,15 @@
 import React, { useState } from "react";
 import "../../styles/HomePage/HomeMainContainer/CarouselHome.css";
 import Carousel from "react-bootstrap/Carousel";
+import DefaultCoverPhoto from "../../assets/profile/user_profile_default_cover.svg";
 
 function CarouselHome({ CarouselImgs }) {
-  // console.log(CarouselImgs.length);
 
   let carouselLenght = CarouselImgs.length;
   if (carouselLenght > 1) carouselLenght = false;
   else carouselLenght = true;
 
   const [index, setIndex] = useState(0);
-
   const imgURL = "https://obscure-ridge-13663.herokuapp.com/user/fetch/";
 
   const handleSelect = (selectedIndex, e) => {
@@ -23,7 +22,7 @@ function CarouselHome({ CarouselImgs }) {
         <img
           key={img.toString()}
           className="d-block w-100 ImgCarouselHome"
-          src={imgURL + img}
+          src={DefaultCoverPhoto}
           alt="slide"
         />
       </Carousel.Item>
@@ -39,7 +38,7 @@ function CarouselHome({ CarouselImgs }) {
       {carouselLenght ? (
         <img
           className="d-block w-100 ImgCarouselHome"
-          src={imgURL + CarouselImgs[0]}
+          src={DefaultCoverPhoto}
           alt="slide"
           style={{
             height: "auto",
