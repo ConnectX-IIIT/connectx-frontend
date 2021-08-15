2d6d9dc3c9d99b676a9ad7bcc12c176106e81a87
Ujjawal gupta <imt_2020107@iiitm.ac.in>
2021-08-15 16:28:23 +0530

    link join us button and firebase update

--- src/components/Landing_Components/Button.js
+++ src/components/Landing_Components/Button.js
@@ -1,10 +1,13 @@
 import React from "react";
 import "../../styles/Landing/Button.css";
+import { Link } from "react-router-dom";
 
 function Button() {
   return (
     <div>
+       <Link to="/signup">
       <button className="Join_us_button">Join Us -{`>`} </button>
+      </Link>
     </div>
   );
 }
