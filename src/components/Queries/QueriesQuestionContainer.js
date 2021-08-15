0100a6ffc4c45ffc25f3fb58cf583e0577262473
HarshilMendpara <mendparaharshil@gmail.com>
2021-08-15 15:29:35 +0530

    added fetched questions in home page

--- src/components/Queries/QueriesQuestionContainer.js
+++ src/components/Queries/QueriesQuestionContainer.js
@@ -1,18 +1,27 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import CreatePostInput from "./../CreatePost/CreatePostInput";
 import ProfilePhoto from "../../assets/profile/user_profile_default_icon.svg";
 import SortComponent from "./SortComponent";
 import HomePageCard from "./../HomePageComponents/HomePageCard";
 import { useStateValue } from "../../helper/state_provider";
+import Cookies from "js-cookie";
+import instance from "../../helper/axios";
+import { useHistory } from "react-router-dom";
 
 const imgURL = "https://obscure-ridge-13663.herokuapp.com/user/fetch/";
 
 function QueriesQuestionContainer() {
 
   const [{ userDetails }, dispatch] = useStateValue(false);
+  const history = useHistory();
+  const [questionData, setQuestionData] = useState([]);
   const [UserQueries, setUserQueries] = useState({
     askedQuestion: "",
   });
+
+  useEffect(() => {
+    fetchQuestionData();
+  }, []);
 
   const handleInput = (e) => {
     const name = e.target.name;
@@ -27,6 +36,48 @@ function QueriesQuestionContainer() {
     return ProfilePhoto;
   };
 
+  const fetchQuestionData = async (e) => {
+    try {
+      const token = Cookies.get("token");
+
+      if (token) {
+        const getQuestionsRes = await instance.get(`/question/getquestions/1`, {
+          headers: {
+            Authorization: `${token}`,
+          },
+        });
+
+        const questions = getQuestionsRes.data.questions;
+        setQuestionData(questions);
+
+      } else {
+        history.replace("/signin");
+      }
+    } catch (error) {
+      if (error.response.status === 500) {
+        return alert(`Server error occured!`);
+      }
+      return alert(`Your session has expired, please login again!`);
+    }
+  };
+
+  const HomePageQuestionsList = questionData.map((item, index) => {
+    return (
+      <HomePageCard
+        UserProfilePhoto={item.userProfile}
+        TimeStamp={item.timestamp}
+        PostUserName={item.userName}
+        PostContent={item.question}
+        PostImageUrls={[]}
+        Upvotes={item.upvotes}
+        PostId="1"
+        isDiscussionQueries={true}
+        queriesInnerStyle={{ fontWeight: "600", fontFamily: "manrope" }}
+        queriesMainContainerStyle={{ marginLeft: "0" }}
+      />
+    );
+  });
+
   return (
     <div>
       <div
@@ -36,7 +87,7 @@ function QueriesQuestionContainer() {
         }}
       >
         <img
-          src={handlePhoto(userDetails.profilePicture)}
+          src={ProfilePhoto}
           alt="profile"
           className="object-cover w-11 h-11"
         />
@@ -59,55 +110,7 @@ function QueriesQuestionContainer() {
       </div>
 
       <SortComponent />
-
-      <HomePageCard
-        UserProfilePhoto={ProfilePhoto}
-        TimeStamp={1223355499}
-        PostUserName="Raj Noobda"
-        PostContent="What Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc non posuere neque, a tincidunt sem?"
-        PostImageUrls={[]}
-        Upvotes="12"
-        PostId="1"
-        isDiscussionQueries={true}
-        queriesInnerStyle={{ fontWeight: "600", fontFamily: "manrope" }}
-        queriesMainContainerStyle={{ marginLeft: "0" }}
-      />
-      <HomePageCard
-        UserProfilePhoto={ProfilePhoto}
-        TimeStamp={1223355499}
-        PostUserName="Raj Noobda"
-        PostContent="What Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc non posuere neque, a tincidunt sem?"
-        PostImageUrls={[]}
-        Upvotes="12"
-        PostId="1"
-        isDiscussionQueries={true}
-        queriesInnerStyle={{ fontWeight: "600", fontFamily: "manrope" }}
-        queriesMainContainerStyle={{ marginLeft: "0" }}
-      />
-      <HomePageCard
-        UserProfilePhoto={ProfilePhoto}
-        TimeStamp={1223355499}
-        PostUserName="Raj Noobda"
-        PostContent="What Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc non posuere neque, a tincidunt sem?"
-        PostImageUrls={[]}
-        Upvotes="12"
-        PostId="1"
-        isDiscussionQueries={true}
-        queriesInnerStyle={{ fontWeight: "600", fontFamily: "manrope" }}
-        queriesMainContainerStyle={{ marginLeft: "0" }}
-      />
-      <HomePageCard
-        UserProfilePhoto={ProfilePhoto}
-        TimeStamp={1223355499}
-        PostUserName="Raj Noobda"
-        PostContent="What Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc non posuere neque, a tincidunt sem?"
-        PostImageUrls={[]}
-        Upvotes="12"
-        PostId="1"
-        isDiscussionQueries={true}
-        queriesInnerStyle={{ fontWeight: "600", fontFamily: "manrope" }}
-        queriesMainContainerStyle={{ marginLeft: "0" }}
-      />
+      {HomePageQuestionsList}
     </div>
   );
 }
