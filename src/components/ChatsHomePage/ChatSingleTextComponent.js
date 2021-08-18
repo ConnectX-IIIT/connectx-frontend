100e28799501ed330016d78fc23a0e2458f9b830
Akash Gupta <akash.gupta.anup@gmail.com>
2021-08-18 13:57:25 +0530

    Added the RefernceMesaage

--- src/components/ChatsHomePage/ChatSingleTextComponent.js
+++ src/components/ChatsHomePage/ChatSingleTextComponent.js
@@ -1,12 +1,25 @@
 import React from "react";
 import "../../styles/Chats/ChatSingleTextComponent.css";
 
-function ChatSingleTextComponent({ isRight }) {
+function ReferenceMessage() {
+  return (
+    <div className="ReferenceMessageChatSection">
+      <h2 className="font-semibold text-lg">Raj Noobda</h2>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit
+      tenetur, asperiores dolorem quisquam pariatur voluptatem officiis dolores
+      atque natus ullam?
+    </div>
+  );
+}
+
+function ChatSingleTextComponent({ isRight, isReference = true }) {
   return (
     <div
       className="ChatSingleTextComponent"
       style={isRight ? { marginLeft: "auto" } : { marginRight: "auto" }}
     >
+      <h2 className="font-semibold text-lg mb-2">Raj Noobda</h2>
+      {isReference ? <ReferenceMessage /> : null}
       Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas libero
       voluptates error! Repudiandae, assumenda! Maiores praesentium dolores
       dolor hic doloremque debitis? Quod, corrupti sint reiciendis hic provident
