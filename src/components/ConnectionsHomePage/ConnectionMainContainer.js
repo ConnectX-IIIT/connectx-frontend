6ecae0769cc47d6b322ab29742c36f099bc93136
HarshilMendpara <mendparaharshil@gmail.com>
2021-08-20 22:33:03 +0530

    added fetched users in connections of home
    Co-authored-by: Ujjawal Gupta <UjjawalGupta30@users.noreply.github.com>

--- src/components/ConnectionsHomePage/ConnectionMainContainer.js
+++ src/components/ConnectionsHomePage/ConnectionMainContainer.js
@@ -1,22 +1,45 @@
-import React from "react";
+import Cookies from "js-cookie";
+import React, { useEffect, useState } from "react";
 import "../../styles/Connection/ConnectionMainContainer.css";
 import ConnectionIndividualComponent from "./ConnectionIndividualComponent";
+import instance from "../../helper/axios";
 
 function ConnectionMainContainer() {
+
+  const [userData, setUserData] = useState([]);
+
+  async function fetchData() {
+    try {
+      const token = Cookies.get("token");
+
+      if (token) {
+        const getDetailsRes = await instance.get(`/home/fetchusers`, {
+          headers: {
+            Authorization: `${token}`,
+          },
+        });
+
+        const userDetails = await getDetailsRes.data.userData;
+        setUserData(userDetails)
+      }
+    } catch (error) {
+      if (error.response.status === 500) {
+        return alert(`Server error occured!`);
+      }
+      return alert(`Your session has expired, please login again!`);
+    }
+  }
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   return (
     <div className="ConnectionMainContainer">
       <div className="ConnectionMainContainerInnerContainer">
-        <ConnectionIndividualComponent />
-        <ConnectionIndividualComponent />
-        <ConnectionIndividualComponent />
-        <ConnectionIndividualComponent />
-        <ConnectionIndividualComponent />
-        <ConnectionIndividualComponent />
-        <ConnectionIndividualComponent />
-        <ConnectionIndividualComponent />
-        <ConnectionIndividualComponent />
-        <ConnectionIndividualComponent />
-        <ConnectionIndividualComponent />
+        {userData.map((item, index) => (
+          <ConnectionIndividualComponent user={item} />
+        ))}
       </div>
     </div>
   );
