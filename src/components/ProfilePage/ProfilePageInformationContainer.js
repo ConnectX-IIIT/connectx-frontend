fe35b7cc8e62c069693f8246de811d48c8000856
Akash Gupta <akash.gupta.anup@gmail.com>
2021-08-27 16:53:39 +0530

    Update ProfilePageInformationContainer.js

--- src/components/ProfilePage/ProfilePageInformationContainer.js
+++ src/components/ProfilePage/ProfilePageInformationContainer.js
@@ -6,11 +6,26 @@ function ProfilePageInformationContainer() {
   return (
     <div className="profile-page-information-container-wrapper">
       <div className="profile-page-information-container">
-        <div>Jaxson Aminoff</div>
+        <div>Jaxson AminofF</div>
         <div></div>
         <div>IPG-M.Tech</div>
         <div></div>
         <div>2015-2020</div>
+      </div>
+      <div className="profile-page-description-container">
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ut dicta
+        repellat totam quam quaerat voluptatum perspiciatis debitis quod error
+        at?
+      </div>
+      <div
+        style={{
+          background: "#FAFFF5",
+          borderColor: "#5DAD0D",
+          color: "#5DAD0D",
+        }}
+        className="profile-page-year-batch"
+      >
+        Alumni
       </div>
     </div>
   );
