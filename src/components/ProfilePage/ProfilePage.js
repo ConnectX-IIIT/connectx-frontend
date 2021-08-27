bb1161a9170c49a00026fc99d475b7fd1c657d20
Akash Gupta <akash.gupta.anup@gmail.com>
2021-08-27 16:53:31 +0530

    Update ProfilePage.js

--- src/components/ProfilePage/ProfilePage.js
+++ src/components/ProfilePage/ProfilePage.js
@@ -1,15 +1,35 @@
-import React, { useState } from "react";
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 import ProfilePageImageContainer from "./ProfilePageImageContainer";
 import ProfilePageInformationContainer from "./ProfilePageInformationContainer";
 
 import "../../styles/ProfilePage/ProfilePage.css";
+import ProfilePageNavbar from "./ProfilePageNavbar";
+import ProfilePagePost from "./ProfilePagePost";
+import ProfilePageQuestion from "./ProfilePageQuestion";
+import ProfilePageAnswer from "./ProfilePageAnswer";
 
 function ProfilePage() {
   return (
     <div className="profile-page-wrapper">
       <ProfilePageImageContainer />
       <ProfilePageInformationContainer />
+
+      <Router>
+        <ProfilePageNavbar />
+        <Switch>
+          <Route path="/home/userprofile/post" component={ProfilePagePost} />
+          <Route
+            path="/home/userprofile/question"
+            component={ProfilePageQuestion}
+          />
+          <Route
+            path="/home/userprofile/answer"
+            component={ProfilePageAnswer}
+          />
+        </Switch>
+      </Router>
     </div>
   );
 }
