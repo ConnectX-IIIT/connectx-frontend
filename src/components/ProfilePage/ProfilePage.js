d6be269d7b45a02f1c57d3be1b1120d7617f5f23
Akash Gupta <akash.gupta.anup@gmail.com>
2021-08-27 12:12:59 +0530

    Update ProfilePage.js

--- src/components/ProfilePage/ProfilePage.js
+++ src/components/ProfilePage/ProfilePage.js
@@ -1,41 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
+
+import ProfilePageImageContainer from "./ProfilePageImageContainer";
+import ProfilePageInformationContainer from "./ProfilePageInformationContainer";
+
 import "../../styles/ProfilePage/ProfilePage.css";
-
-import photoIcon from "../../assets/profile_page/ic_camera.svg";
-import DefaultCoverPhoto from "../../assets/_rough/achi photo part 2.jpg";
-import DefaultProfilePhoto from "../../assets/_rough/Jethalal-1200.jpg";
-import photoIconWhite from "../../assets/profile_page/ic_camera_white.svg";
-
-function ProfilePageImageContainer() {
-  return (
-    <div className="profile-page-image-container">
-      <img
-        src={DefaultCoverPhoto}
-        alt="cover"
-        className="profile-page-cover-photo"
-      />
-      <div className="profile-page-upload-photo">
-        <img src={photoIcon} alt="photo" />
-        <div>Update cover photo</div>
-      </div>
-      <div className="profile-page-profile-photo-wrapper">
-        <img
-          src={DefaultProfilePhoto}
-          alt="cover"
-          className="profile-page-profile-photo"
-        />
-        <div className="profile-page-profile-photo-edit">
-          <img src={photoIconWhite} alt="photo" />
-        </div>
-      </div>
-    </div>
-  );
-}
 
 function ProfilePage() {
   return (
     <div className="profile-page-wrapper">
       <ProfilePageImageContainer />
+      <ProfilePageInformationContainer />
     </div>
   );
 }
