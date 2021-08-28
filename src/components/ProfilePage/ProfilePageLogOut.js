41182e9fa6820b3b6c13da19994e4c950e31a81c
Ujjawal gupta <imt_2020107@iiitm.ac.in>
2021-08-28 17:08:30 +0530

    designed logout

---
+++ src/components/ProfilePage/ProfilePageLogOut.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import "../../styles/ProfilePage/LogOut.css";
+const ProfilePageLogOut = () => {
+    return (
+        <div>
+            <button className="logOutBttn">
+                <div className="logOut">Log out</div>
+                </button>
+        </div>
+    )
+}
+
+export default ProfilePageLogOut
