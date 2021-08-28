dd60ac6a48e33fe35e471744e3f18c43d47da62b
Ujjawal gupta <imt_2020107@iiitm.ac.in>
2021-08-28 15:03:55 +0530

    changed classname

--- src/components/ChatsHomePage/GroupMembers.js
+++ src/components/ChatsHomePage/GroupMembers.js
@@ -3,7 +3,7 @@ import DefaultProfilePhoto from "../../assets/_rough/Jethalal-1200.jpg";
 import "../../styles/Chats/GroupMembers.css";
 const GroupMembers = () => {
     return (
-        <div className="groupmembers">
+        <div className="memberBorder">
             <div className="MemberName">Jethalal</div>
             <div className="MemberInfo">Lorem ipsum, dolor sit amet consectetur ...</div>
             <img src={DefaultProfilePhoto} className="MembersProfile"></img>
