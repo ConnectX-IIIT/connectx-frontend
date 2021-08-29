4cfad7e6d53768041a40ac565520d9eadef21223
HarshilMendpara <mendparaharshil@gmail.com>
2021-08-29 12:26:57 +0530

    added handleSubmit function
    Co-authored-by: Ujjawal Gupta <UjjawalGupta30@users.noreply.github.com>

--- src/components/HomePageComponents/ButtonHome.js
+++ src/components/HomePageComponents/ButtonHome.js
@@ -1,15 +1,28 @@
 import React from "react";
+import { useStateValue } from "../../helper/state_provider";
 import "../../styles/HomePage/HomeMainContainer/ButtonHome.css";
-function ButtonHome({ content, styleButton, jobLink }) {
+function ButtonHome({ content, styleButton, jobLink, filter }) {
+
+  const [{ userDetails, postFilter }, dispatch] = useStateValue();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    if (jobLink) {
+      window.open(jobLink, '_blank');
+    }
+
+    if (filter) {
+      await dispatch({
+        type: "UPDATE_POST_FILTER",
+        filter,
+      })
+    }
+  }
+
   return (
     <button
-      onClick={jobLink ? (e) => {
-        e.preventDefault();
-        window.open(
-          jobLink,
-          '_blank'
-        );
-      } : null}
+      onClick={handleSubmit}
       type="submit"
       className="ButtonHome"
       style={styleButton}>
