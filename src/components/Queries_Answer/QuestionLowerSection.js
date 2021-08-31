6890422f17cf66628040626807ea8f45256a4948
Akash Gupta <akash.gupta.anup@gmail.com>
2021-08-31 19:53:24 +0530

    Create QuestionLowerSection.js

---
+++ src/components/Queries_Answer/QuestionLowerSection.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+import "../../styles/Question/QuestionLowerSection.css";
+import QuestionSectionCard from "./QuestionSectionCard";
+
+function QuestionLowerSection() {
+  return (
+    <div className="question-section-lower-wrapper">
+      <QuestionSectionCard />
+    </div>
+  );
+}
+
+export default QuestionLowerSection;
