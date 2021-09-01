b287eb08d1537866e3e66f0c7833020d4cbd6c3c
Akash Gupta <akash.gupta.anup@gmail.com>
2021-09-01 19:07:22 +0530

    Update QuestionSectionCard.js

--- src/components/Queries_Answer/QuestionSectionCard.js
+++ src/components/Queries_Answer/QuestionSectionCard.js
@@ -6,6 +6,9 @@ import "../../styles/Question/QuestionSectionCard.css";
 import QuestionSectionUserprofile from "./QuestionSectionUserprofile";
 import UpvotesSection from "./UpvotesSection";
 import HomeCardInnerContent from "./../HomePageComponents/HomeCardInnerContent";
+import QuestionSectionButtons from "./QuestionSectionButtons";
+
+import QuestionSectionDiscussionSection from "./QuestionSectionDiscussionSection";
 
 function QuestionSectionCard() {
   return (
@@ -14,19 +17,30 @@ function QuestionSectionCard() {
         <UpvotesSection />
       </div>
       <div className="question-section-card-right-wrapper">
-        <QuestionSectionUserprofile
-          UserName="Harshil Piro"
-          TimeStamp="15:34 PM"
-          ProfilePhoto={DefaultProfile}
-        />
+        <div className="question-section-card-user-profile">
+          <QuestionSectionUserprofile
+            UserName="Harshil Piro"
+            TimeStamp="15:34 PM"
+            ProfilePhoto={DefaultProfile}
+          />
+        </div>
         <div className="question-section-read-more-wrapper">
           <HomeCardInnerContent
             styleInnerContent={{
               lineHeight: "initial",
             }}
-            InnerContent="Hello"
+            InnerContent="Hello Everyone"
           />
         </div>
+        <div className="question-section-card-bottom-wrapper">
+          <div>
+            <QuestionSectionButtons buttonType="comment" />
+          </div>
+          <div>
+            <QuestionSectionButtons />
+          </div>
+        </div>
+        <QuestionSectionDiscussionSection />
       </div>
     </div>
   );
