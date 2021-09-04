14d59b6cae47e56b56703c63d74e150ab364ebc2
Ujjawal gupta <imt_2020107@iiitm.ac.in>
2021-09-04 20:36:16 +0530

    created admin pannel

---
+++ src/components/Admin.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import AdminPannelHeaderContainer from "./Admin_Components/AdminPannelHeaderContainer"
+import VerificationMainContainer from './Admin_Components/VerificationMainContainer'
+
+const Admin = () => {
+    return (
+        <div>
+            <AdminPannelHeaderContainer />
+            <VerificationMainContainer/>
+        </div>
+    )
+}
+
+export default Admin
