8d4e960b9b5fdbd4c566227acb6d3438822ba708
Raj varsani <varasaniraj5@gmail.com>
2021-09-07 17:33:04 +0530

    Added discussionButtonCompData fle

---
+++ src/components/HomePageComponents/helper/bottom_buttons_data/postDiscussionButtonData.js
@@ -0,0 +1,22 @@
+import { ReactComponent as DefaultSVGComp } from "../../../../assets/home/post/bottom/ic_dicussion.svg";
+
+let discussionButtonCompData = {
+  svgComponent: <DefaultSVGComp />,
+  buttonName: "Discussion",
+  colorsSet: {
+    activeColors: {
+      primary: "#00CA51",
+      secondary: "#E8FFEB",
+    },
+    inactiveColors: {
+      primary: "#222222",
+      secondary: "#F5F5F5",
+    },
+    hoverColors: {
+      primary: "#28894F",
+      secondary: "#F6FFF7",
+    },
+  },
+};
+
+export default discussionButtonCompData;
