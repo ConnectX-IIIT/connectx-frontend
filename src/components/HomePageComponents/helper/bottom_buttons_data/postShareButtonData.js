8e66f1d78bbece4826c703b9bdb86a5348f1ef7f
Raj varsani <varasaniraj5@gmail.com>
2021-09-07 18:32:55 +0530

    Added postShareButtonData

---
+++ src/components/HomePageComponents/helper/bottom_buttons_data/postShareButtonData.js
@@ -0,0 +1,22 @@
+import { ReactComponent as DefaultSVGComp } from "../../../../assets/home/post/bottom/ic_share.svg";
+
+let shareButtonCompData = {
+  svgComponent: <DefaultSVGComp />,
+  buttonName: "Share",
+  colorsSet: {
+    activeColors: {
+      primary: "#1792DD",
+      secondary: "#E5F5FF",
+    },
+    inactiveColors: {
+      primary: "#222222",
+      secondary: "#F5F5F5",
+    },
+    hoverColors: {
+      primary: "#64C2FC",
+      secondary: "#F2FAFF",
+    },
+  },
+};
+
+export default shareButtonCompData;
