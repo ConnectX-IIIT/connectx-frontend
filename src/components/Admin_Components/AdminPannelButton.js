d27f462cc0f78ad629f543393e5474ab8d89ed1d
Akash Gupta <akash.gupta.anup@gmail.com>
2021-09-08 10:39:09 +0530

    Update AdminPannelButton.js

--- src/components/Admin_Components/AdminPannelButton.js
+++ src/components/Admin_Components/AdminPannelButton.js
@@ -1,11 +1,33 @@
-import React from 'react'
+import React, { useState } from "react";
+import "../../styles/Admin/AdminPannelButton.css";
 
-const AdminPannelButton = () => {
-    return (
-        <div>
-            
-        </div>
-    )
+function AdminPannelButton({ innerContent, buttonColor, paddingX, isBorder }) {
+  const [buttonHover, setButtonHover] = useState(false);
+  return (
+    <button
+      onMouseOver={() => {
+        setButtonHover(!buttonHover);
+      }}
+      onMouseLeave={() => {
+        setButtonHover(!buttonHover);
+      }}
+      style={{
+        color: isBorder ? (buttonHover ? "white" : buttonColor) : buttonColor,
+        paddingLeft: paddingX,
+        paddingRight: paddingX,
+        border: isBorder ? "1px solid" : "none",
+        borderColor: buttonColor,
+        backgroundColor: isBorder
+          ? buttonHover
+            ? buttonColor
+            : "transparent"
+          : "transparent",
+      }}
+      className="admin-pannel-bottom-button"
+    >
+      {innerContent}
+    </button>
+  );
 }
 
-export default AdminPannelButton
+export default AdminPannelButton;
