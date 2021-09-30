d522913b2b3fb17aec669284c3fe29ecc9f55c1c
Raj <varasaniraj5@gmail.com>
2021-09-30 15:08:59 +0530

    Removed garbage import from ChatIndividual.js

--- src/components/ChatsHomePage/ChatIndividual.js
+++ src/components/ChatsHomePage/ChatIndividual.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useRef } from "react";
+import React, { useEffect, useState } from "react";
 import { useStateValue } from "../../helper/state_provider";
 import "../../styles/Chats/ChatIndividual.css";
 import { format } from "timeago.js";
