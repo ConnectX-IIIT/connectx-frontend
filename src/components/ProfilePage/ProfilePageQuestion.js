107e92a00e6ed6b0243f247bc5e7c2458163b8ec
Raj <varasaniraj5@gmail.com>
2021-09-30 18:59:16 +0530

    Revert "Resolving Warnings in ProfilePagePost.js"
    This reverts commit 0123ca199b17632f0c50a6f50157f368b4477b65.

--- src/components/ProfilePage/ProfilePageQuestion.js
+++ src/components/ProfilePage/ProfilePageQuestion.js
@@ -5,14 +5,15 @@ import HomePageCard from "../HomePageComponents/HomePageCard";
 import { fetchQuestion } from "../Queries_Answer/helper/fetch_question";
 
 function ProfilePageQuestion(props) {
+
   const history = useHistory();
-  const [, dispatch] = useStateValue();
+  const [{ userDetails }, dispatch] = useStateValue();
   const [questionData, setQuestionData] = useState([]);
   const userId = props.match.params.userId;
 
   useEffect(() => {
     fetchQuestion(history, false, false, userId, setQuestionData);
-  }, []); // eslint-disable-line react-hooks/exhaustive-deps
+  }, []);
 
   const handleQuestionClick = (question) => async (e) => {
     e.preventDefault();
@@ -38,11 +39,7 @@ function ProfilePageQuestion(props) {
           UserId={item.user}
           isDiscussionQueries={true}
           onQuestionClick={handleQuestionClick(item)}
-          queriesInnerStyle={{
-            fontWeight: "600",
-            fontFamily: "manrope",
-            cursor: "pointer",
-          }}
+          queriesInnerStyle={{ fontWeight: "600", fontFamily: "manrope", cursor: "pointer" }}
           queriesMainContainerStyle={{ marginLeft: "0" }}
         />
       </div>
