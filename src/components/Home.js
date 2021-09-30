4708652ba1ef377aa2682bc21b7e7946b2ec55a0
Raj <varasaniraj5@gmail.com>
2021-09-30 15:42:53 +0530

    Resolving Warnings in Home.js >> Removed garbage imports

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import { Switch, Route, useHistory } from "react-router-dom";
 import Navbar from "./HomePageComponents/Navbar";
 import HomeMainContainer from "../components/HomePageComponents/HomeMainContainer";
